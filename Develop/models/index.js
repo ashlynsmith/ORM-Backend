5d9c6cadc53145007d5670e930dd708cd6a9dd69
Ashlyn Smith <SMITHASHLYN210@YAHOO.COM>
2023-06-09 10:22:05 -0500

    models fixed

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,19 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, {});
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Categories have many Products
-Category.hasMany(Product, {});
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
+  onDelete: 'SET NULL'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
+  onDelete: 'SET NULL'
 });
 
 
